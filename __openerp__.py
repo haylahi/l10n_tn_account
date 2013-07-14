1f6171d762203dbbc2b922b7b7089dbd4a843339
haylahi <cplus.contact@gmail.com>
2013-07-15 00:04:39 +0100

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    account_group_invoice_lines module for OpenERP, Change method to group invoice lines in account
+#    Copyright (C) 2013 CREATIVE PLUS)
+#              Haythem Lahmadi <cplus.contact@gmail.com>
+#
+#    This file is a part of account_group_invoice_lines
+#
+#    account_group_invoice_lines is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    account_group_invoice_lines is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Tunisian Account Customize',
+    'version': '1.0',
+    "category": 'Accounting & Finance',
+    'complexity': "easy",
+    'description': """Add some feature for Tunisian Accounting 
+    """,
+    'author': 'Haythem Lahmadi',
+    'website': 'cplus.contact@gmai.com',
+    'depends': [
+        'base',
+        'account',
+    ],
+
+    'data': [
+        
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
